5aae799810cfdc062867837c3bbed535c6b9c674
alicehua11 <43593804+alicehua11@users.noreply.github.com>
2019-04-28 13:33:53 -0700

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+
+	$("#s1").click(function(){
+		$("#o1").fadeIn(200);
+		});
+
+	$("#s2").click(function(){
+		$(".outfit").hide();
+		$("#o2").fadeIn(200);
+		});
+
+	$("#s3").click(function(){
+		$(".outfit").hide();	
+		$("#o3").fadeIn(200);
+		});
+
+	$("#s4").click(function(){
+		$(".outfit").hide();
+		$("#o4").fadeIn(200);
+		});
+
+	$("#s5").click(function(){
+		$(".outfit").hide();
+		$("#o5").fadeIn(200);
+		});
+	$("#strip-button").click(function(){
+		$(".outfit").hide();
+	});
+
+});
